8a932cfd5c33ebadb982f197b9c6f601837d0e0c
hanjuntao <hanjuntao@hd123.com>
2019-06-25 09:03:29 +0800

    Update test

--- demo-spring-boot-starter-test/src/main/java/me/mingshan/spring/boot/starter/test/Application.java
+++ demo-spring-boot-starter-test/src/main/java/me/mingshan/spring/boot/starter/test/Application.java
@@ -2,11 +2,13 @@ package me.mingshan.spring.boot.starter.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author mingshan
  */
 @SpringBootApplication
+@ComponentScan(basePackages = {"me.mingshan.spring.boot"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
